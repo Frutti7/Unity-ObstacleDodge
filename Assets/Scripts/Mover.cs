d50b6b9292128ca82c2a84155cef59e51f91e31c
Patrick Ouimet <pat_117@hotmail.com>
2026-09-08 12:07:08 -0400

    Added Time.DeltaTime with moveSpeed

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
+    [SerializeField] float moveSpeed = 10.0f;
+    
     void Start() {
     }
 
 
     void Update() {
     
-    float xValue = Input.GetAxis("Horizontal");
+    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
     float yValue = 0.00f;
-    float zValue = Input.GetAxis("Vertical");
+    float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
     transform.Translate(xValue, yValue, zValue);
     }
 
