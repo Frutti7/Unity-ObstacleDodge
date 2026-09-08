50ce5b7622f4b494f80d12ddd7e280b937e5d7c1
Patrick Ouimet <pat_117@hotmail.com>
2026-09-08 09:53:38 -0400

    Added 3 variables

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
+    private float xValue = 0f;
+    private float yValue = 0f;
+    private float zValue = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
     }
 
     // Update is called once per frame
     void Update() {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
