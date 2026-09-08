b986e6d5b3c760de76537797b782c6b1cfae481c
Patrick Ouimet <pat_117@hotmail.com>
2026-09-08 11:37:55 -0400

    Added keyboard movement (old input)

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
-    [SerializeField] private float xValue = 0f;
-    [SerializeField] private float yValue = 0.004f;
-    [SerializeField] private float zValue = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
     }
 
-    // Update is called once per frame
+
     void Update() {
-        transform.Translate(xValue, yValue, zValue);
+    
+    float xValue = Input.GetAxis("Horizontal");
+    float yValue = 0.00f;
+    float zValue = Input.GetAxis("Vertical");
+    transform.Translate(xValue, yValue, zValue);
     }
+
 }
