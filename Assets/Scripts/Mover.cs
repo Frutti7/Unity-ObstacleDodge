6bc8e4278fb3a872a7be0410e8cca2749d4c4c9c
Patrick Ouimet <pat_117@hotmail.com>
2026-09-09 10:51:43 -0400

    17. Added basic collision

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class Mover : MonoBehaviour {
     [SerializeField] float moveSpeed = 10.0f;
+    //[SerializeField] private CinemachineCamera cinemachine;
+    //private CinemachineFollow compo;
     
     void Start() {
+        //compo = cinemachine.GetComponent<CinemachineFollow>();
     }
 
 
@@ -13,5 +17,8 @@ public class Mover : MonoBehaviour {
     float yValue = 0.00f;
     float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
     transform.Translate(xValue, yValue, zValue);
+    //compo.FollowOffset.y += 0.02f;
     }
+    
+    
 }
