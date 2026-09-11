89aa4ad7143ca8eba0ebbb866beadbf90b018565
Patrick Ouimet <pat_117@hotmail.com>
2026-09-11 12:41:26 -0400

    Added Time.time
    Used to show time elapsed since the beginning of the game in seconds
    (float)

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Update() {
+      float timeElapsed = Time.time;
+      Debug.Log(Time.time + " seconds has passed since the beginning of the game");
+   }
+}
