134f0de80de80167157af5c7369264dc5bb9db96
Patrick Ouimet <pat_117@hotmail.com>
2026-09-12 21:29:37 -0400

    Added dropping after 3 seconds

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-    [SerializeField] float timeToDrop = 3f; 
+    [SerializeField] float timeToWait = 2f;
+    MeshRenderer myMeshRenderer;
+    Rigidbody myRigidbody;
+    
+    void Start() {
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidbody = GetComponent<Rigidbody>();
+        myMeshRenderer.enabled = false;
+        myRigidbody.useGravity = false;
+    }
+    
     void Update() {
-        
-        if (Time.time > timeToDrop) {
-            Debug.Log("Dropped");
+        if (Time.time > timeToWait) {
+            myMeshRenderer.enabled = true;
+            myRigidbody.useGravity = true;
         }
-      
    }
 }
